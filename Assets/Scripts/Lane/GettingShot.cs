5c6a24f7459f88fe64537893a994d092f7feb4a3
Korbin <klnevius@gmail.com>
2023-10-02 17:48:58 -0400

    bullet

--- Assets/Scripts/Lane/GettingShot.cs
+++ Assets/Scripts/Lane/GettingShot.cs
@@ -1,15 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Ship;
 using UnityEngine;
 
 public class GettingShot : MonoBehaviour
 {
+   private Player _player;
+
+   private void Awake()
+   {
+      _player = GetComponent<Player>();
+   }
+
    private void OnTriggerEnter(Collider other)
    {
       if (other.CompareTag("Bullet"))
       {
-         Destroy(gameObject);
+         var bullet = other.GetComponentInParent<Bullet>();
+         if (bullet.theShooter != _player)
+         {
+            //hit by bullet that i did not shoot
+            Destroy(gameObject);
+         }
       }
    }
 }
