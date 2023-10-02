df6ab2c8c9f82cbc4d7b41ee767abc2ef1540212
Supererogato <terrariaprogamer2.0@gmail.com>
2023-10-02 15:32:18 -0400

    Comments for teleport script

--- Assets/Scenes/Ethan/TeleportScript.cs
+++ Assets/Scenes/Ethan/TeleportScript.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class TeleportScript : MonoBehaviour
 {
 
+    //target location is public for ease of editor access to the transform
     public Transform targetLocation;
 
     // public bool isExit;
     public float distance = 0.2f;
 
+    
+    //Below code for an entrance and an exit. Simply comment out if you don't want the player to go back through
     private void Start()
     {
         // if (isExit == false)
@@ -23,6 +26,7 @@ public class TeleportScript : MonoBehaviour
         // }
     }
 
+    //Finds the transform of a public object and uses that as an anchor for teleporting positions
     void OnTriggerEnter(Collider other)
     {
         if (Vector3.Distance(transform.position, other.transform.position) > distance)
